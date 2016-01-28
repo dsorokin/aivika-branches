b559f09090dcf9ddd0b7be1d4e161e3090ad0752
David Sorokin <david.sorokin@gmail.com>
2016-01-28 06:55:07 +0300

    Updated the mutable reference.

--- Simulation/Aivika/Branch/Internal/Ref.hs
+++ Simulation/Aivika/Branch/Internal/Ref.hs
@@ -21,6 +21,8 @@ module Simulation.Aivika.Branch.Internal.Ref
         writeRef,
         modifyRef) where
 
+import Debug.Trace
+
 import Data.IORef
 import qualified Data.IntMap as M
 
@@ -34,7 +36,7 @@ import Simulation.Aivika.Trans.Internal.Types
 import Simulation.Aivika.Branch.Internal.Br
 
 -- | A reference map.
-type RefMap a = IORef (M.IntMap (Weak (IORef a)))
+type RefMap a = IORef (M.IntMap (IORef a))
 
 -- | A mutable reference.
 data Ref a = Ref { refMap :: RefMap a,
@@ -56,8 +58,8 @@ newEmptyRef0 =
   Br $ \ps ->
   do rm <- newIORef M.empty
      wm <- mkWeakIORef rm $
-           do m <- readIORef rm
-              forM_ (M.elems m) finalize
+           trace "fin newEmptyRef0" $
+           atomicModifyIORef rm $ \m -> (M.empty, ())
      return Ref { refMap = rm,
                   refWeakMap = wm }
 
@@ -73,9 +75,9 @@ newRef0 a =
      ra <- newIORef a
      let !i  = brId ps
          !wm = refWeakMap r
-     wa <- mkWeak ps ra (Just $ finalizeRef wm i)
+     mkWeakIORef (brIdGenerator ps) (trace "fin newIORef0" $ finalizeRef wm i)
      writeIORef (refMap r) $
-       M.insert i wa M.empty
+       M.insert i ra M.empty
      return r
      
 -- | Read the value of a reference.
@@ -86,11 +88,7 @@ readRef r =
   do m <- readIORef (refMap r)
      let loop ps =
            case M.lookup (brId ps) m of
-             Just wa ->
-               do ra <- deRefWeak wa
-                  case ra of
-                    Just ra -> readIORef ra
-                    Nothing -> error "The reference was finalized: readRef"
+             Just ra -> readIORef ra
              Nothing ->
                case brParent ps of
                  Just ps' -> loop ps'
@@ -105,17 +103,13 @@ writeRef r a =
   do m <- readIORef (refMap r)
      let !i = brId ps
      case M.lookup i m of
-       Just wa ->
-         do ra <- deRefWeak wa
-            case ra of
-              Just ra -> a `seq` writeIORef ra a
-              Nothing -> error "The reference was finalized: writeRef"
+       Just ra -> a `seq` writeIORef ra a
        Nothing ->
          do ra <- a `seq` newIORef a
             let !wm = refWeakMap r
-            wa <- mkWeak ps ra (Just $ finalizeRef wm i)
+            mkWeakIORef (brIdGenerator ps) (trace "fin writeRef" $ finalizeRef wm i)
             atomicModifyIORef (refMap r) $ \m ->
-              let m' = M.insert i wa m in (m', ())
+              let m' = M.insert i ra m in (m', ())
 
 -- | Mutate the contents of the reference.
 modifyRef :: Ref a -> (a -> a) -> Event BrIO ()
@@ -125,14 +119,10 @@ modifyRef r f =
   do m <- readIORef (refMap r)
      let !i = brId ps
      case M.lookup i m of
-       Just wa ->
-         do ra <- deRefWeak wa
-            case ra of
-              Just ra ->
-                do a <- readIORef ra
-                   let b = f a
-                   b `seq` writeIORef ra b
-              Nothing -> error "The reference was finalized: modifyRef"
+       Just ra ->
+         do a <- readIORef ra
+            let b = f a
+            b `seq` writeIORef ra b
        Nothing ->
          do a <- invokeBr ps $ invokeEvent p $ readRef r
             invokeBr ps $ invokeEvent p $ writeRef r (f a)
@@ -141,15 +131,16 @@ modifyRef r f =
 finalizeRef :: Weak (RefMap a) -> Int -> IO ()
 finalizeRef wm i =
   do rm <- deRefWeak wm
+     trace ("finalizeRef: " ++ show i) $
+       return ()
      case rm of
        Nothing ->
          return ()
        Just rm ->
          do m <- readIORef rm
             case M.lookup i m of
-              Just wa ->
-                do atomicModifyIORef rm $ \m ->
-                     let m' = M.delete i m in (m', ())
-                   finalize wa
+              Just ra ->
+                atomicModifyIORef rm $ \m ->
+                let m' = M.delete i m in (m', ())
               Nothing ->
                 return ()
